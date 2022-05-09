38e58e65552de7a9188d58230086976e240afd72
arlw87 <arlw87@googlemail.com>
2022-05-09 22:51:47 +0100

    User Entity Added to Data Model. Using Lombok for getters and setters.

---
+++ src/main/java/com/code/arlw87/restaurantReviewApi/Model/User.java
@@ -0,0 +1,39 @@
+package com.code.arlw87.restaurantReviewApi.Model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name="USERS")
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    @Column(name="DISPLAY_NAME")
+    private String displayName;
+
+    @Column(name="CITY")
+    private String city;
+
+    @Column(name="COUNTY")
+    private String county;
+
+    @Column(name="POSTCODE")
+    private String postCode;
+
+    @Column(name="PEANUT_ALLERGY")
+    private Boolean peanutAllergy;
+
+    @Column(name="EGG_ALLERGY")
+    private Boolean eggAllergy;
+
+    @Column(name="DIARY_ALLERGY")
+    private Boolean diaryAllergy;
+
+}
