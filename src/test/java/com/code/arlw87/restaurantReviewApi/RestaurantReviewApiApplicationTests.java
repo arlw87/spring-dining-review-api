40326bdf0396687bd1e7d61abd0a0e62838222df
arlw87 <arlw87@googlemail.com>
2022-05-09 19:27:02 +0100

    Add Spring Boot Initializr Files with Spring JPA, Spring Web and H2 Database dependencies

---
+++ src/test/java/com/code/arlw87/restaurantReviewApi/RestaurantReviewApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.arlw87.restaurantReviewApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantReviewApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
