1cdc6d9e02d3e0623c1fb2406c8beed37504857d
arlw87 <arlw87@googlemail.com>
2022-05-09 23:14:14 +0100

    Add Review Entity in the data model. Closes #3

---
+++ src/main/java/com/code/arlw87/restaurantReviewApi/Model/Review.java
@@ -0,0 +1,35 @@
+package com.code.arlw87.restaurantReviewApi.Model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name="REVIEWS")
+public class Review {
+
+    @Id
+    @GeneratedValue
+    private Integer Id;
+
+    @Column(name="REVIEW_SUBMITTED_BY")
+    private String reviewSubmittedBy;
+
+    @Column(name="RESTUARANT_REVIEWED_ID")
+    private Integer restuarantReviewedId;
+
+    @Column(name="PEANUT_SCORE")
+    private Integer peanutScore;
+
+    @Column(name="EGG_SCORE")
+    private Integer eggScore;
+
+    @Column(name="DIARY_SCORE")
+    private Integer diaryScore;
+
+    @Column(name="COMMENTS")
+    private String comments;
+}
